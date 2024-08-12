1c498659b29943021514e0cec5fe5093927a398c
stephenmwise <162519976+stephenmwise@users.noreply.github.com>
2024-08-12 17:30:04 -0400

    Updated header text parameters

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import pandas as pd
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 df = pd.read_csv("topics.csv")
 
-#PAGE BEGIN
-pdf.add_page()
+for index, row in df.iterrows():
+    #PAGE BEGIN
+    pdf.add_page()
 
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello There!", align="L",
-         ln=1)
-pdf.set_font(family="Times", size=10)
-pdf.cell(w=0, h=12, txt="Hi!", align="L",
-         ln=1)
-#PAGE END
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(10, 22, 200, 22)
+
+    #PAGE END
 
 #Creates output file as PDF
 pdf.output("output.pdf")
