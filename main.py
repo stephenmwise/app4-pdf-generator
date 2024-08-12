93e5fe4e0a0ac8b06adc4e905612e94eb1aabb15
stephenmwise <162519976+stephenmwise@users.noreply.github.com>
2024-08-12 18:13:41 -0400

    Added: Lines every 10mm on each page

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 22, 200, 22)
+    for y in range(24, 290, 10):
+        pdf.line(10, y, 200, y)
 
     # Adds footer
     pdf.ln(265)
@@ -26,7 +27,8 @@ for index, row in df.iterrows():
     # Adds additional pages to each section
     for i in range(row["Pages"] - 1):
         pdf.add_page()
-
+        for y in range(20, 290, 10):
+            pdf.line(10, y, 200, y)
         # Adds footer
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
